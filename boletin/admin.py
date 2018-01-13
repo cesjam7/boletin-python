0fc5d6d79b343a75ac0e02f7032f3b5f1a12033e
Cesar Aquino Maximiliano <cesar7492@gmail.com>
2018-01-12 23:25:33 -0500

    modificando vista del admin modelo

--- boletin/admin.py
+++ boletin/admin.py
@@ -6,5 +6,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Registrado
 
+class AdminRegistrado(admin.ModelAdmin):
+    list_display = ['email', 'nombre', 'timestamp']
+    # list_display_links = ['nombre']
+    list_filer = ['timestamp']
+    list_editable = ['nombre'] # No puede ser el primer campo de list_display a menos que definas que el link sea otro
+    search_fields = ['email', 'nombre']
+    class Meta:
+        model = Registrado
 
-admin.site.register(Registrado)
+admin.site.register(Registrado, AdminRegistrado)
